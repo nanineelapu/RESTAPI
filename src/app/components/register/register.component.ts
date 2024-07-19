45f06fb5cfdf5f6dd393bddf301739ee32d009be
Nani Neelapu <neelapunani2580@gmail.com>
2024-07-20 00:24:55 +0530

    Updated RestApi Project

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import {
+  FormsModule,
+  FormControl,
+  FormGroup,
+  FormControlName,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-
+  registerForm = new FormGroup({
+    fname: new FormControl(' ', Validators.required),
+    lname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confrimpassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+  constructor() {}
+  ngOnInit() {}
 }
